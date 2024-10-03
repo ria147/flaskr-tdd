aa4ee797d781c8cc9d9e17a37bfc7c70324ea0e0
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-02 22:15:23 -0400

    Add code to pass test 1

--- project/app.py
+++ project/app.py
@@ -1,3 +1,17 @@
+from flask import Flask
+
+
+# create and initialize a new Flask app
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run()
 # import os
 # from functools import wraps
 # from pathlib import Path
