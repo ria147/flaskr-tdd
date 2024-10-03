88bea61f50293b848db66dced74ce074040ddd28
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-03 00:52:49 -0400

    SQLAlchemy Setup

--- create_db.py
+++ create_db.py
@@ -2,6 +2,7 @@
 
 
 from project.app import app, db
+from project.models import Post
 
 
 with app.app_context():
@@ -10,3 +11,16 @@ with app.app_context():
 
     # commit the changes
     db.session.commit()
+
+# # create_db.py
+
+
+# from project.app import app, db
+
+
+# with app.app_context():
+#     # create the database and the db table
+#     db.create_all()
+
+#     # commit the changes
+#     db.session.commit()
