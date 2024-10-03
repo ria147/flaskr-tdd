fdef4f7cf78003c7e94f2ab7ce1a7c58b31fd29b
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-02 22:30:18 -0400

    Add test 2 and code for it to pass - database

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,5 @@
-from project.app import app
+from pathlib import Path
+from project.app import app, init_db
 
 
 def test_index():
@@ -7,6 +8,11 @@ def test_index():
 
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+def test_database():
+    init_db()
+    assert Path("flaskr.db").is_file()
+
 # import json
 # from pathlib import Path
 
